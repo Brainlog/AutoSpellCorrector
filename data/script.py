16c1c5eba360b6827de923fc4ba310c2dbf40fc1
Vatsal-2020CS50449 <vatsaljingar.iitd@gmail.com>
2022-09-21 15:27:45 +0530

    segment local search

--- data/script.py
+++ data/script.py
@@ -1,11 +1,16 @@
-fil = open("output.txt","r")
-fil2 = open("pred.txt","r")
-s1 = fil.read()
-s2 = fil2.read()
-diff=0
-count = 0
-for i in range(len(s1)):
-    if s1[i]==s2[i]:
-        diff+=1
-    count += 1    
-print(f'{(diff/count)*100}% CHARACTERS MATCH...\n\n')
+fil = open("pred.txt","r")
+fil2 = open("output.txt","r")
+j = 1
+while j <= 15:
+    s1 = fil.readline()
+    s2 = fil2.readline()
+    print(f's1: {s1}')
+    print(f's2: {s2}')
+    diff=0
+    count = 0
+    for i in range(len(s2)):
+        if s1[i]==s2[i]:
+            diff+=1
+        count += 1    
+    print(f'{(diff/count)*100}% CHARACTERS MATCH...\n\n')
+    j += 1
