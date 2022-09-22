ec9b24dd07d7c56febfae4be4115dc628c2caa72
Stiti-create <77821734+Stiti-create@users.noreply.github.com>
2022-09-22 11:54:34 +0530

    Add files via upload

--- script.py
+++ script.py
@@ -1,17 +1,22 @@
 fil = open("data/pred.txt","r")
 fil2 = open("data/output.txt","r")
-j = 1
-while j <= 15:
+j = 0
+accur = 0
+while True:
     s1 = fil.readline()
     s2 = fil2.readline()
-    print(f's1: {s1}')
-    print(f's2: {s2}')
+    if(s1=='\n' or s2=='\n'):
+        print('END OF TEST CASES')
+        print(f'TOTAL ACCURACY FOR {j} TEST CASES: {accur/j}%')
+        break
     diff=0
     count = 0
     for i in range(len(s2)):
         if s1[i]==s2[i]:
             diff+=1
-        count += 1    
-    print(f'{(diff/count)*100}% CHARACTERS MATCH...')
+        count += 1  
+    curracc=  (diff/count)*100
     j += 1
+    print(f'ACCURACY {j}: {curracc}% CHARACTERS MATCH')
+    accur += curracc
     
